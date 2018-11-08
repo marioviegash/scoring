8fdae512a6025336acf6844199d0b56121f603e9
Mario Viegash <marioviegash@binusian.org>
2018-11-08 23:19:38 +0700

    Add Home Template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,12 +28,16 @@
 @endsection
 
 @section('content')
-    @include('shared.header')
-	<!-- BEGIN CONTAINER -->
-	<div class="page-container">
-		@include('shared.sidebar')
-	</div>
-    @include('shared.footer')
+    <body class="page-header-fixed page-sidebar-closed-hide-logo page-content-white">
+        <div class="page-wrapper">
+            @include('shared.header')
+            <!-- BEGIN CONTAINER -->
+            <div class="page-container">
+                @include('shared.sidebar')
+            </div>
+            @include('shared.footer')
+        </div>
+    </body>
 @endsection
 
 @section('script')
