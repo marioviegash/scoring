5fbcba3d5b62d272899d8c3a0a9da54361286621
Mario Viegash <marioviegash@binusian.org>
2018-11-16 00:55:23 +0700

    Remove useless js

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,10 +2,60 @@
 <html>
 <head>
     <title>@yield('title')</title>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('template_admin/assets/global/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('template_admin/assets/global/plugins/simple-line-icons/simple-line-icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('template_admin/assets/global/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('template_admin/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="{{ asset('template_admin/assets/global/css/components.min.css') }}" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="{{ asset('template_admin/assets/global/css/plugins.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <link href="{{ asset('template_admin/assets/layouts/layout/css/layout.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('template_admin/assets/layouts/layout/css/themes/darkblue.min.css') }}" rel="stylesheet" type="text/css" id="style_color" />
+    <link href="{{ asset('template_admin/assets/layouts/layout/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END THEME LAYOUT STYLES -->
     @yield('style')
 </head>
 <body>
 @yield('content')
+
+<!-- BEGIN CORE PLUGINS -->
+<script src="{{ asset('template_admin/assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/global/plugins/js.cookie.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="{{ asset('template_admin/assets/global/plugins/moment.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/global/plugins/morris/morris.min.js') }}" type="text/javascript"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="{{ asset('template_admin/assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="{{ asset('template_admin/assets/pages/scripts/dashboard.min.js') }}" type="text/javascript"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="{{ asset('template_admin/assets/layouts/layout/scripts/layout.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/layouts/layout/scripts/demo.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('template_admin/assets/layouts/global/scripts/quick-nav.min.js') }}" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
+<script>
+    $(document).ready(function()
+    {
+        $('#clickmewow').click(function()
+        {
+            $('#radio1003').attr('checked', 'checked');
+        });
+    })
+</script>
 @yield('script')
 </body>
 </html>
