37dd6a4c8627ebedb4f97b269c8aa7317ffdf5a7
Mario Viegash <marioviegash@binusian.org>
2018-11-15 23:10:13 +0700

    Add Header For Another Verification Page

--- resources/views/register/friend-one.blade.php
+++ resources/views/register/friend-one.blade.php
@@ -1,14 +1,15 @@
 @extends('register.layout.master')
 
 @section('content')
-    @include('shared.header')
-    @include('register.shared.header')
+    @include('register.shared.progress')
     <fieldset>
-        <h2 class="fs-title">INVITE YOUR FRIEND 1</h2>
-        <h3 class="fs-subtitle">Step 3</h3>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="email" name="email" placeholder="Email" />
-        <input type="button" name="previous" class="previous action-button" value="Previous" />
-        <input type="button" name="next" class="next action-button" value="Next" />
+        <form action="">
+            <h2 class="fs-title">INVITE YOUR FRIEND 1</h2>
+            <h3 class="fs-subtitle">Step 3</h3>
+            <input type="text" name="name" placeholder="Name" />
+            <input type="email" name="email" placeholder="Email" />
+            <input type="button" name="previous" class="previous action-button" value="Previous" />
+            <input type="submit" name="next" class="next action-button" value="Next" />
+        </form>
     </fieldset>
 @endsection
--- resources/views/register/profile.blade.php
+++ resources/views/register/profile.blade.php
@@ -1,15 +1,16 @@
 @extends('register.layout.master')
 
 @section('content')
-    @include('shared.header')
-    @include('register.shared.header')
+    @include('register.shared.progress')
     <fieldset>
-        <h2 class="fs-title">COMPLETE YOUR PROFILE</h2>
-        <h3 class="fs-subtitle">Step 2</h3>
-        <input type="text" name="position" placeholder="Position" />
-        <input type="text" name="work_place" placeholder="Work Place" />
-        <input type="text" name="c_level" placeholder="C Level" />
-        <input type="button" name="previous" class="previous action-button" value="Previous" />
-        <input type="button" name="next" class="next action-button" value="Next" />
+        <form action="">
+            <h2 class="fs-title">COMPLETE YOUR PROFILE</h2>
+            <h3 class="fs-subtitle">Step 2</h3>
+            <input type="text" name="position" placeholder="Position" />
+            <input type="text" name="work_place" placeholder="Work Place" />
+            <input type="text" name="c_level" placeholder="C Level" />
+            <input type="button" name="previous" class="previous action-button" value="Previous" />
+            <input type="submit" name="next" class="next action-button" value="Next" />
+        </form>
     </fieldset>
 @endsection
