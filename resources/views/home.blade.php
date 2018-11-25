bc43c2f482ecd4607bb97b06cf3518b805b46413
Mario Viegash <marioviegash@binusian.org>
2018-11-25 13:41:38 +0700

    Add New Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,73 @@
             <!-- BEGIN CONTAINER -->
             <div class="page-container">
                 @include('shared.sidebar')
+
+                <div class="page-content-wrapper">
+                    <div class="page-content">
+                        <div class="page-bar">
+                            <ul class="page-breadcrumb">
+                                <li>
+                                    <a href="index.html">Home</a>
+                                    <i class="fa fa-circle"></i>
+                                </li>
+                                <li>
+                                    <span>Dashboard</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <h1 class="page-title"> Admin Dashboard
+                            <small>Reports</small>
+                        </h1>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="portlet light bordered">
+                                    <div class="portlet-title">
+                                        <div class="caption">
+                                            <i class="icon-user font-green"></i>
+                                            <span class="caption-subject font-green bold uppercase">Group Table</span>
+                                        </div>
+                                    </div>
+                                    <div class="portlet-body">
+                                        <div class="table-scrollable">
+                                            <table class="table table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th> # </th>
+                                                        <th> Group Name </th>
+                                                        <th> Participant 1 </th>
+                                                        <th> Participant 2 </th>
+                                                        <th> Participant 3 </th>
+                                                        <th> Status </th>
+                                                        <th> Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td> 1 </td>
+                                                        <td> Group Alay </td>
+                                                        <td> tedjun@gmail.com </td>
+                                                        <td> tedjun@gmail.com </td>
+                                                        <td> tedjun@gmail.com </td>
+                                                        <td>
+                                                            <span class="label label-sm label-info"> Pending </span>
+                                                        </td>
+                                                        <td>
+                                                            <button type="button" class="btn btn-default">Approve</button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
             @include('shared.footer')
         </div>
     </body>
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -39,6 +39,7 @@
 <script src="{{ asset('template_admin/assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
 <!-- END THEME GLOBAL SCRIPTS -->
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
+@yield('script')
 <script src="{{ asset('template_admin/assets/pages/scripts/dashboard.min.js') }}" type="text/javascript"></script>
 <!-- END PAGE LEVEL SCRIPTS -->
 <!-- BEGIN THEME LAYOUT SCRIPTS -->
@@ -56,6 +57,5 @@
         });
     })
 </script>
-@yield('script')
 </body>
 </html>
