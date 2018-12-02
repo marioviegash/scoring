7f72968f66f747e9151c852aa2848217823cd9a6
Mario Viegash <marioviegash@binusian.org>
2018-12-02 16:40:40 +0700

    Add Route for Page Verification

--- resources/views/register/shared/header.blade.php
+++ resources/views/register/shared/header.blade.php
@@ -1,11 +1,11 @@
 <div class="header">
     <div class="header-logo">
-        <a href="index.html">
+        <a href="/">
             <img src="{{ asset('template_admin/assets/layouts/layout/img/logo.png') }}" alt="logo" class="logo-default"/>
         </a>
     </div>
     <div class="header-menu">
-        <a href="">
+        <a href="/logout">
             <i class="icon-logout"></i>
         </a>
     </div>
