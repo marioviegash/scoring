831771a76dfadd1a13e825102e10ec512123bd0e
Mario Viegash <marioviegash@gmail.com>
2019-01-07 23:15:46 +0700

    Add Asset Logo and Mockup

--- resources/views/pages/judgement/detail.blade.php
+++ resources/views/pages/judgement/detail.blade.php
@@ -3,7 +3,10 @@
 @section('title', 'Home - Scoring')
 
 @section('style')
-
+    <link href="{{ asset('template_admin/assets/global/plugins/ion.rangeslider/css/ion.rangeSlider.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('template_admin/assets/global/plugins/ion.rangeslider/css/ion.rangeSlider.skinFlat.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('template_admin/assets/global/plugins/bootstrap-markdown/css/bootstrap-markdown.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('template_admin/assets/global/plugins/bootstrap-summernote/summernote.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 
 @section('content')
@@ -40,8 +43,18 @@
                                         <span class="caption-subject font-green bold uppercase">Group Name</span>
                                     </div>
                                 </div>
-                                <div class="portlet-body">
-                                </div>
+                                <form class="portlet-body form">
+                                    <form role="form" class="form-horizontal form-bordered">
+                                        <div class="form-body">
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Basic</label>
+                                                <div class="col-md-9">
+                                                    <input id="range_1" type="text" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -54,5 +67,9 @@
 @endsection
 
 @section('script')
-
+    <script src="{{ asset('template_admin/assets/global/plugins/ion.rangeslider/js/ion.rangeSlider.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('template_admin/assets/global/plugins/bootstrap-markdown/lib/markdown.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('template_admin/assets/global/plugins/bootstrap-markdown/js/bootstrap-markdown.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('template_admin/assets/global/plugins/bootstrap-summernote/summernote.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('template_admin/assets/pages/scripts/components-ion-sliders.min.js') }}" type="text/javascript"></script>
 @endsection
